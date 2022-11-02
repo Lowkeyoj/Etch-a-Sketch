2bf6586f3da38529914725e685e46b1e8c30a60f
Lowkeyoj <rockyojaji@gmail.com>
2022-11-01 17:46:19 -0700

    Perfect squares fill up grid nicely

--- java.js
+++ java.js
@@ -2,19 +2,24 @@ const div = document.querySelector('.first');
 
 const btn = document.querySelector('.class');
 
+const input = document.querySelector('.text');
+
 
 
 
 function createDivs(par){
+
+div.style.gridTemplateRows = `repeat(${Math.sqrt(par)}, 2fr)`;
+div.style.gridTemplateColumns = `repeat(${Math.sqrt(par)}, 2fr)`;
+
 for(i = 0;i < par; i++){
 const divs = document.createElement('div');
 divs.classList.add('try');
-divs.style.backgroundColor = 'green';
-divs.style.height = '16px';
-divs.style.width = '16px';
-divs.style.display ='grid';
+divs.style.boxSizing = 'border-box';
+divs.style.backgroundColor = 'blue';
+divs.style.borderStyle = 'solid';
+div.appendChild(divs)
 
-div.appendChild(divs);
 }
 
 }
