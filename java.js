24251f4629a566c6d3b2ce3142ce03db2e12aca4
Lowkeyoj <rockyojaji@gmail.com>
2022-11-12 11:14:30 -0800

    template literrals so that i could change the colors;

--- java.js
+++ java.js
@@ -1,42 +1,58 @@
 const div = document.querySelector('.first');
 
-const btn = document.querySelector('.class');
+const btn = document.getElementById('btn');
 
-const input = document.querySelector('.text');
+const input = document.getElementById('myText');
 
 const divs = document.createElement('div');
+ 
+let color = 'red';
+let mouse = 'mouseover';
 
 
+function createDivs(par){
+if(par > 90){
+  par = 16;
+}
+div.style.gridTemplateRows = `repeat(${par}, 1fr)`;
+div.style.gridTemplateColumns = `repeat(${par}, 1fr)`;
 
-
-
-function createDivs(){
-
-div.style.gridTemplateRows = `repeat(${Math.sqrt(256)}, 1fr)`;
-div.style.gridTemplateColumns = `repeat(${Math.sqrt(256)}, 1fr)`;
-
-for(i = 0;i < 256; i++){    
+for(i = 0;i < Math.pow(par,2); i++){    
 const divs = document.createElement('div');
 divs.setAttribute('id','items');
 divs.style.boxSizing = 'border-box';
 divs.style.borderStyle = 'solid';
-divs.addEventListener('mouseover', function() {
- divs.style.backgroundColor = 'red'; 
-})
+divs.style.borderWidth = 'thin';
+divs.addEventListener(`${mouse}`, function() {
+divs.style.backgroundColor = `${color}`; 
+});
+divs.addEventListener('click', function() {
+  divs.style.backgroundColor = 'green'; 
+  });
+
 div.appendChild(divs);  
   }
   
 }
- 
+
+let cl = () =>{
+  createDivs(input.value)
+  btn.disabled = true; 
+}
 
 
 
+let er = () =>{
+  color = 'white';
+}
 
-btn.addEventListener('click', () => {
-  // let par = prompt('How big do you want the grid');
-//    par =  
-    createDivs();
-} )
+let bl = () =>{
+  color = 'blue';
+}
+
+let R = () =>{
+color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}`
+}
 
 
 
